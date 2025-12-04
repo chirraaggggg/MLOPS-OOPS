96d3cc8e2f43e2e224e3f8ad7c9589506670b29c
Chirag <chhirag920@gmail.com>
2025-12-04 14:26:15 +0530

    pushing my code

--- 1.py
+++ 1.py
@@ -1,13 +1,30 @@
-class employee:
-    # special method/magic method/dunder method - constructor
-    def __init__(self):
-        self.id = 123
-        self.salary = 500000
-        self.designation = "SDE"
+# class employee:
+#     # special method/magic method/dunder method - constructor
+#     def __init__(self):
+#         self.id = 123
+#         self.salary = 500000
+#         self.designation = "SDE"
 
-# create an obj/instance of the class
+# # create an obj/instance of the class
 
-sam = employee()
+# sam = employee()
 
-print(sam.id)
-print(sam.salary)
+# print(sam.id)
+# print(sam.salary)
+
+# class student:
+#     name = "John"
+
+# s1 = student()
+# print(s1.name)
+
+# s2 = student()
+# print(s2.name)
+
+class car:
+    color = "blue"
+    brand = "mercedes"
+
+car1 = car()
+print(car1.brand)
+print(car1.color)
